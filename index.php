d82eb99bad5cff63d1768265fb2733dff83f06c8
Jofadodo <jofadodo@gmail.com>
2022-08-27 14:51:16 +0200

    slider zaciatok

--- index.php
+++ index.php
@@ -2,15 +2,84 @@
 
 <main>
 
-    <section id="div1">
+    <!-- <section id="slider"> -->
 
-    </section>
+        <section class="css-slider-wrapper">
+            <input type="radio" name="slider" class="slide-radio1" checked id="slider_1">
+            <input type="radio" name="slider" class="slide-radio2" id="slider_2">
+            <input type="radio" name="slider" class="slide-radio3" id="slider_3">
+            <input type="radio" name="slider" class="slide-radio4" id="slider_4">
 
-    <section id="div2">
+            <!-- Slider Pagination -->
+            <div class="slider-pagination">
+                <label for="slider_1" class="page1"></label>
+                <label for="slider_2" class="page2"></label>
+                <label for="slider_3" class="page3"></label>
+                <label for="slider_4" class="page4"></label>
+            </div>
 
-    </section>
+            <!-- Slider #1 -->
+            <div class="slider slide-1">
+                <!-- <img src="img/slider/IMG_7231.jpg" alt=""> -->
+                    <!-- <div class="slider-content">
+                        <h4>New Product</h4>
+                        <h2>Denim Longline T-Shirt Dress With Split</h2>
+                        <button type="button" class="buy-now-btn" name="button">$130</button>
+                    </div> -->
+                <div class="number-pagination">
+                    <span>1</span>
+                </div>
+            </div>
 
-    
+            <!-- Slider #2 -->
+            <div class="slider slide-2">
+                <!-- <img src="img/slider/IMG_7255-2.jpg" alt=""> -->
+                    <!-- <div class="slider-content">
+                        <h4>New Product</h4>    
+                        <h2>Denim Longline T-Shirt Dress With Split</h2>
+                        <button type="button" class="buy-now-btn" name="button">$130</button>
+                    </div> -->
+                <div class="number-pagination">
+                    <span>2</span>
+                </div>
+            </div>
+
+            <!-- Slider #3 -->
+            <div class="slider slide-3">
+                <!-- <img src="img/slider/IMG_7320-2.jpg" alt=""> -->
+                    <!-- <div class="slider-content">
+                        <h4>New Product</h4>
+                        <h2>Denim Longline T-Shirt Dress With Split</h2>
+                        <button type="button" class="buy-now-btn" name="button">$130</button>
+                    </div> -->
+                <div class="number-pagination">
+                    <span>3</span>
+                </div>
+            </div>
+
+            <!-- Slider #4 -->
+            <div class="slider slide-4">
+                <img src="img/slider/IMG_7231.jpg" alt="">
+                <div class="number-pagination">
+                    <span>4</span>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- </section> -->
+        
+        <section class="">
+            
+        </section>
+            
+        <section id="div2">
+                
+        </section>
+                
+                
+                <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" charset="utf-8"></script> -->
+                <script src="js/app.js" charset="utf-8"></script>
 </main>
 
 <?php include 'shared/footer.php' ?>
